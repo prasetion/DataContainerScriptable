53cca68906581adf9dfee19bcbce75f3a2e7e7d5
prasetion <prasetion@gmail.com>
2018-02-24 15:59:17 +0700

    create asset scriptable object
    add ScriptableData.cs for create data container

---
+++ DataContainer/Assets/Scripts/ScriptableData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="HitData", menuName="HitAsset")]
+public class ScriptableData : ScriptableObject {
+
+	public string attackType;
+	public float hitDamage;
+
+}
