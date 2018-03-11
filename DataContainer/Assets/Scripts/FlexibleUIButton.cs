e6bc8ff3bcbe4d6c95212ede08875d49f185f90a
prasetion <prasetion@gmail.com>
2018-03-12 00:45:16 +0700

    update button type

--- DataContainer/Assets/Scripts/FlexibleUIButton.cs
+++ DataContainer/Assets/Scripts/FlexibleUIButton.cs
@@ -7,13 +7,24 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class FlexibleUIButton : FlexibleUI {
 
+	public enum ButtonType{
+		Default,
+		Confirm,
+		Decline,
+		Warning
+	}
+
 	Image _image;
+	Image _icon;
 	Button _button;
+
+	public ButtonType buttonType;
 
 	protected override void OnSkinUI(){
 		base.OnSkinUI();
 
 		_image = GetComponent<Image>();
+		_icon = _image.GetComponentInChildren<Image>();
 		_button = GetComponent<Button>();
 
 		_button.transition = Selectable.Transition.SpriteSwap;
@@ -22,6 +33,30 @@ public class FlexibleUIButton : FlexibleUI {
 		_image.sprite = skinData.buttonSprite;
 		_image.type = Image.Type.Sliced;
 		_button.spriteState = skinData.buttonSpriteState;
+
+
+		switch(buttonType){
+
+			case ButtonType.Default:
+				_image.color = skinData.defaultColor;
+				_icon.sprite = skinData.defaultIcon;
+				break;
+
+			case ButtonType.Confirm:
+				_image.color = skinData.confirmColor;
+				_icon.sprite = skinData.defaultIcon;
+				break;
+
+			case ButtonType.Decline:
+				_image.color = skinData.declineColor;
+				_icon.sprite = skinData.declineIcon;
+				break;
+			
+			case ButtonType.Warning:
+				_image.color = skinData.warningColor;
+				_icon.sprite = skinData.warningIcon;
+				break;
+		}
 
 	}
 
