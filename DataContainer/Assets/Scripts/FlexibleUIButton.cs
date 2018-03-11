357799db0cd214693d24092bfc7ecc8efb05a5df
prasetion <prasetion@gmail.com>
2018-03-12 00:28:43 +0700

    editor, flexible UI, edit scriptable object data

---
+++ DataContainer/Assets/Scripts/FlexibleUIButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+public class FlexibleUIButton : FlexibleUI {
+
+	Image _image;
+	Button _button;
+
+	protected override void OnSkinUI(){
+		base.OnSkinUI();
+
+		_image = GetComponent<Image>();
+		_button = GetComponent<Button>();
+
+		_button.transition = Selectable.Transition.SpriteSwap;
+		_button.targetGraphic = _image;
+
+		_image.sprite = skinData.buttonSprite;
+		_image.type = Image.Type.Sliced;
+		_button.spriteState = skinData.buttonSpriteState;
+
+	}
+
+}
